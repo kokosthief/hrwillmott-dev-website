296f77d38f9a4956a48891342cdd3358b14b7b13
hrwillmott <indihank@gmail.com>
2023-03-07 12:45:41 +0000

    New Build

---
+++ constants/data.js
@@ -0,0 +1,120 @@
+const userData = {
+  githubUsername: "kokosthief",
+  name: "Henry Willmott",
+  designation: "Web3 Dev (in training)",
+  avatarUrl: "/avatar.png",
+  email: "hello@hrwillmott.com",
+  phone: "+31683421604",
+  address: "Amsterdam, The Netherlands",
+  projects: [
+    {
+      title: "Tally - App",
+      link: "https://tourmaline-crisp-cd625c.netlify.app/",
+      imgUrl: "https://github.com/kokosthief/Tally-App-Upgrade/raw/main/Screenshot-of-app.png",
+    },
+    {
+      title: "Meerscan - Memecoin",
+      link: "https://meerscan.org",
+      imgUrl: "/meerscan-website.jpeg",
+    },
+    {
+      title: "DeFi-Robot - Website",
+      link: "https://portal.defirobot.org/",
+      imgUrl: "/drbt-website.jpeg",
+    },
+    {
+      title: "This Portfolio",
+      link: "https://hrwillmott.com",
+      imgUrl: "/portfoliodev.png",
+    },
+    {
+      title: "DeFi-Robot - Documentation",
+      link: "https://docs.defirobot.org/",
+      imgUrl: "/drbtbanner.png",
+    },
+    {
+      title: "A-Bike Spain",
+      link: "https://a-bike.es",
+      imgUrl: "/a-bike-spain.jpeg",
+    },
+    {
+      title: "Roetz-Bikes Landing Page",
+      link: "https://web.archive.org/web/20210715154451/https://roetz-bikes.com/",
+      imgUrl: "/roetz.gif",
+    },
+  ],
+  about: {
+    title:
+      "I like the challenge and I get to contribute to cool projects",
+    description: [
+      `What's important for me in life is to be reflective, open and continuously developing. is to continue learning with interest. Development and crypto interests me the most.`,
+      `My creative background has served me well, from making films to running ads, as now I get to combine design and the new found love challenge of coding as I aim to be a web3 dev.`,
+      `I'm involved with crypto projects and love the space. Therefore very excited by this career trajectory.`,
+    ],
+    currentProject: "DeFi-Robot Website",
+    currentProjectUrl: "https://portal.defirobot.org/",
+  },
+  experience: [
+    {
+      title: "Web3 Developer (in-training)",
+      company: "HRW",
+      year: "2022",
+      companyLink: "https://portal.defirobot.org/",
+      desc: "Whilst I train as a dev, I've already made websites by getting involved with numerouse crypto projects and got into community management, technical documentation and more...",
+    },
+    {
+      title: "Ecstatic Dance DJ",
+      company: "HRW",
+      year: "2021",
+      companyLink: "https://soundcloud.com/henners-ecstatic-dance",
+      desc: "DJing and facilitating the transformative practice of Ecstatic Dance.",
+    },
+    {
+      title: "Digital Marketing Analyst",
+      company: "Roetz-Bikes",
+      year: "2021",
+      companyLink: "https://roetz-bikes.com/",
+      desc: "My first introduction of HTML, CSS & Javascript. Loved every part of the job working on a common goal of circularity.",
+    },
+    {
+      title: "Start-up Marketer",
+      company: "COMMON GROUNDS",
+      year: "2020",
+      companyLink: "https://www.linkedin.com/company/commongroundsco/",
+      desc: "Ads, Growth, marketing stuffz",
+    },
+    {
+      title: "Freelance Creative",
+      company: "HRW",
+      year: "2020",
+      companyLink: "https://www.linkedin.com/company/89639505/admin/",
+      desc: "Filmmaking, photography, video editing, ad campaigns, websites...",
+    },
+    {
+      title: "Graduation",
+      company: "University of Lincoln.",
+      year: "2014",
+      companyLink: "https://www.lincoln.ac.uk/",
+      desc: "Film & TV Production: Upper Second Class Honours (2:1)- Nobody asks this but it's pretty good.",
+    },
+    {
+      title: "High School",
+      company: "Walton High",
+      year: "2010",
+      companyLink: "",
+      desc: "I liked, media, design and sports the most",
+    },
+  ],
+  resumeUrl:
+    "https://docs.google.com/document/d/1BDyOEn-epkepV0qCAoAfisYJHZSisGR0GLgUAflhxJg/edit?usp=sharing",
+  socialLinks: {
+    
+    linkedin: "https://www.linkedin.com/in/henrywillmott/",
+    telegram: "https://t.me/kokosthief",
+    twitter: "https://twitter.com/kokosthief",
+    soundcloud: "https://soundcloud.com/henry-ecstatic-dance",
+    github: "https://github.com/kokosthief"
+  },
+};
+
+export default userData;
